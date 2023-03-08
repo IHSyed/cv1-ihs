b62e14de8dda29f83704903c7b11d96cb3335e79
Iqbal Hussain Syed <65956129+IHSyed@users.noreply.github.com>
2023-03-08 19:31:31 +0500

    Second Commit

---
+++ src/data/data-page.tsx
@@ -0,0 +1,18 @@
+export const personalData = {
+    name: "Iqbal Hussain Syed",
+    status: "Retiree",
+    education: [
+        "B.Sc Electrical Engineering (Power), UET Lahore 1968-1973",
+        "M.Sc Nuclear Technology (PINSTECH), Quaid-E-Azam Unibersity Islamabad 1973-1975 "
+    ],
+    contactLinks: [
+        "mailto: emailto: ihsyedscc@gmail.com",
+        "https://twitter.com/IqbalHussainSy3",
+        "https:// linkedin.com/iqbal-hussain-6100b52a"
+    ],
+};
+
+export const aboutMe = {
+    title: "About Me",
+    body: 
+}
