449a71c226a20ecbca4b91f983791122d99a042b
Iqbal Hussain Syed <65956129+IHSyed@users.noreply.github.com>
2023-03-09 18:41:37 +0500

    Third Commit

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import Image from 'next/image'
+//import {Envelope, Linkedin, Twitter} from "./icons/"
 
-export default function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
-}
+const Sidebar = ({data}) => {
+    const {name, status, education, contactLinks} = data;
+
+//export default function Sidebar() {
+  return <div className='bg-black flex flex-col h-screen'>
+    <div>
+    <Image />
+    <h1> {name}</h1>
+    <h2> {status}</h2>
+    <p> {education[0]}</p>
+    <p> {education[1]}</p>
+    <div>
+    <h3> Contact Me </h3>
+
+    <a href = {contactLinks?.[0]} aria-label = {"email link"} >
+
+        </a>
+    <a href = {contactLinks?.[1]} aria-label = {"twitter link"} >
+
+        </a>
+    <a href = {contactLinks?.[2]} aria-label = {"linkedin link"} >
+    
+        </a>
+
+    </div>
+    </div>
+
+
+    </div>
+};
+
+export default Sidebar;
+
--- src/data/data-page.tsx
+++ src/data/data-page.tsx
@@ -3,7 +3,7 @@ export const personalData = {
     status: "Retiree",
     education: [
         "B.Sc Electrical Engineering (Power), UET Lahore 1968-1973",
-        "M.Sc Nuclear Technology (PINSTECH), Quaid-E-Azam Unibersity Islamabad 1973-1975 "
+        "M.Sc Nuclear Technology (PINSTECH), Quaid-E-Azam University Islamabad 1973-1975 ",
     ],
     contactLinks: [
         "mailto: emailto: ihsyedscc@gmail.com",
@@ -14,5 +14,32 @@ export const personalData = {
 
 export const aboutMe = {
     title: "About Me",
-    body: 
-}
+    body: "Highly experienced in EHV/HV Underground Transmission System Design, Installation and Commissioning",
+};
+
+export const skills = {
+    soft: [
+        
+        {icon: "", text:"Avid Listener"},
+        {icon: "", text:"Effective Comminicator"},
+        {icon: "", text:"Collaboration"},
+        {icon: "", text:"Team Player"},
+        {icon: "", text:"Problem Solver"},
+        {icon: "", text:"Time Management"},
+    ],
+};
+
+export const professionalData = {
+    title: "Professional Experience",
+    experiences: [
+        {
+            role: "Director - Quality Control and Product Pre-Qualification",
+            Organisation: "Saudi Cable Company",
+            serviceYears: "April 2014 - December 2016",
+            jobDescription: "- Planning organizing and managing the provision of a Quality Control service to the production of Power Cable, TCP, PVC & RM, by ensuring having upto standard InHouse Test Laboratories and professional inspection staff, providing inspection and testing at all stages of the manufacturing process and to ensure the achievement of customer requirements and the objectives stated In the Company Mission Statement.",
+             
+        }
+    ]
+
+
+};
